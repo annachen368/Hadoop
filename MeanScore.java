da2792d777b3a883bb8b09196eef6a804c1c3cf9
Anna <annachen368@gmail.com>
2015-11-22 14:53:38 -0800

    fix combine error

--- MeanScore.java
+++ MeanScore.java
@@ -15,7 +15,6 @@ public class MeanScore {
 
   public static class MeanMap extends Mapper<Object, Text, Text, IntWritable>{
 
-    // private final static IntWritable one = new IntWritable(1);
     private Text name = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
@@ -23,12 +22,9 @@ public class MeanScore {
       System.out.println(line);
       StringTokenizer itr = new StringTokenizer(line, "\n");
       while (itr.hasMoreTokens()) {
-        // word.set(itr.nextToken());
-        // context.write(word, one);
         StringTokenizer tokenizerLine = new StringTokenizer(itr.nextToken());
         String strName = tokenizerLine.nextToken();
         String strScore = tokenizerLine.nextToken();
-        // Text name = new Text(strName);
         name.set(strName);
         int scoreInt = Integer.parseInt(strScore);
         context.write(name, new IntWritable(scoreInt));
@@ -42,20 +38,12 @@ public class MeanScore {
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       int sum = 0;
       int count = 0;
-      // Iterator<IntWritable> iterator = values.iterator();
-      // while(iterator.hasNext()) {
       for(IntWritable val : values) {
-        // sum += iterator.next().get();
         sum += val.get();
         count++;
       }
-      // for (IntWritable val : values) {
-      //   sum += val.get();
-      // }
-      // result.set(sum);
-      // context.write(key, result);
+	System.out.println("mycount="+count);
       result.set((int)sum/count);
-      // context.write(key, new IntWritable(avg));
       context.write(key, result);
     }
   }
@@ -65,34 +53,13 @@ public class MeanScore {
     Job job = Job.getInstance(conf, "Mean Score");
     job.setJarByClass(MeanScore.class);
     job.setMapperClass(MeanMap.class);
-    job.setCombinerClass(MeanReduce.class);
+    //job.setCombinerClass(MeanReduce.class);
     job.setReducerClass(MeanReduce.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
-    
-    // int ret = ToolRunner.run(new MeanScore(), args);
-    // System.exit(ret);
   }
 
-  // public int run(String[] args) throws Exception {
-  //   Job job = new Job(getConf());
-  //   job.setJarByClass(MeanScore.class);
-  //   job.setJobName("MeanScore");
-  //   job.setOutputKeyClass(Text.class);
-  //   job.setOutputValueClass(IntWritable.class);
-  //   job.setMapperClass(Map.class);  
-  //   job.setCombinerClass(Reducer.class);
-  //   job.setReducerClass(Reducer.class);
-  //   job.setInputFormatClass(TextInputFormat.class);
-  //   job.setOutputFormatClass(TextOutputFormat.class);
-  //   FileInputFormat.setInputPath(job, new Path(args[0]));
-  //   FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-  //   boolean success = job.waitForCompletion(true);
-  //   return success ? 0 : 1;
-  //   // System.exit(job.waitForCompletion(true) ? 0 : 1);
-  // }
 }
